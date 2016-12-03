65cc3b404eb6fa57230fbb855caf72dd11cef1f6
Vincent Lee <vincent.lee@outlook.com>
2016-12-03 02:37:53 -0600

    d3-2 and privatize things

--- src/advent_of_code_2016/day2.clj
+++ src/advent_of_code_2016/day2.clj
@@ -3,22 +3,22 @@
 
 (def keypad-size 3)
 
-(defn row [pos] (quot (dec pos) keypad-size))
-(defn col [pos] (mod (dec pos) keypad-size))
+(defn- row [pos] (quot (dec pos) keypad-size))
+(defn- col [pos] (mod (dec pos) keypad-size))
 
-(defn up [pos] (if (zero? (row pos)) pos (- pos keypad-size)))
-(defn down [pos] (if (= (row pos) (- keypad-size 1)) pos (+ pos keypad-size)))
-(defn left [pos] (if (zero? (col pos)) pos (dec pos)))
-(defn right [pos] (if (= (col pos) (- keypad-size 1)) pos (inc pos)))
+(defn- up [pos] (if (zero? (row pos)) pos (- pos keypad-size)))
+(defn- down [pos] (if (= (row pos) (- keypad-size 1)) pos (+ pos keypad-size)))
+(defn- left [pos] (if (zero? (col pos)) pos (dec pos)))
+(defn- right [pos] (if (= (col pos) (- keypad-size 1)) pos (inc pos)))
 
-(defn calc-pos [op pos]
+(defn- calc-pos [op pos]
   (condp = op
     \U (up pos)
     \D (down pos)
     \L (left pos)
     \R (right pos)))
 
-(defn process-line [^String line start-pos]
+(defn- process-line [^String line start-pos]
   ; todo we repeat this pattern below, is there a more compact way?
   (loop [[c & xs] line
          pos start-pos]
--- src/advent_of_code_2016/day3.clj
+++ src/advent_of_code_2016/day3.clj
@@ -1,16 +1,34 @@
 (ns advent-of-code-2016.day3
   (:require [clojure.string :as str]))
 
-(defn valid-triangle [[a b c]]
+(defn- valid-triangle [[a b c]]
   (and (> (+ a b) c) (> (+ a c) b) (> (+ b c) a)))
 
-(defn to-triple [^String line]
+(defn- to-triple [^String line]
+  "Turns line into 3-vector"
   (let [[a b c] (re-seq #"\d+" line)]
     [(Long/parseLong a) (Long/parseLong b) (Long/parseLong c)]))
 
-(defn to-triples [^String input]
+(defn- to-triples [^String input]
+  "Turns each line into a 3-vector, returns seq of those"
   (let [lines (str/split input #"\R+")]
     (map to-triple lines)))
 
 (defn day3-1 [^String input]
-    (count (filter valid-triangle (to-triples input))))
+  (->> (to-triples input)
+       (filter valid-triangle)
+       (count)))
+
+(defn- transpose
+  [[[a1 b1 c1]
+    [a2 b2 c2]
+    [a3 b3 c3]]]
+
+  [[a1 a2 a3] [b1 b2 b3] [c1 c2 c3]])
+
+(defn day3-2 [^String input]
+  (->> (to-triples input)
+       (partition 3)
+       (mapcat transpose)
+       (filter valid-triangle)
+       (count)))
