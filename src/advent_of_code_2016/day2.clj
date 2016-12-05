6fd6c398f6607b6152fc3aa5ef7d67974a7ed092
Vincent Lee <vincent.lee@outlook.com>
2016-12-04 20:34:07 -0600

    Don't reimplement reduce

--- src/advent_of_code_2016/day2.clj
+++ src/advent_of_code_2016/day2.clj
@@ -3,30 +3,26 @@
 
 (def keypad-size 3)
 
-; These all take/return 1-indexed positions
+; row/col work on 0-based indices
 (defn- row [pos] (quot (dec pos) keypad-size))
 (defn- col [pos] (mod (dec pos) keypad-size))
 
+; these work on 1-based indices
 (defn- up [pos] (if (zero? (row pos)) pos (- pos keypad-size)))
 (defn- down [pos] (if (= (row pos) (- keypad-size 1)) pos (+ pos keypad-size)))
 (defn- left [pos] (if (zero? (col pos)) pos (dec pos)))
 (defn- right [pos] (if (= (col pos) (- keypad-size 1)) pos (inc pos)))
 
-(defn- calc-pos [op pos]
+(defn- calc-pos [pos op]
   (condp = op
     \U (up pos)
     \D (down pos)
     \L (left pos)
     \R (right pos)))
 
-(defn- process-line [^String line start-pos]
-  ; todo we repeat this pattern below, is there a more compact way?
-  (loop [[c & xs] line
-         pos start-pos]
-    (if (seq xs) (recur xs (calc-pos c pos)) (calc-pos c pos))))
+(defn- process-line [start-pos ^String line]
+  (println start-pos)
+  (reduce calc-pos start-pos line))
 
 (defn day2-1 [^String input]
-  (loop [[line & xs] (str/split-lines input)
-         pos 5]
-    (println pos)
-    (if (seq xs) (recur xs (process-line line pos)) (process-line line pos))))
+  (reduce process-line 5 (str/split-lines input)))
