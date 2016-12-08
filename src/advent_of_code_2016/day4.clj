0c4dcb54ccd55e9634df4d42362903bbeb6ced51
Vincent Lee <vincent.lee@outlook.com>
2016-12-08 14:11:08 -0600

    Finish d4

--- src/advent_of_code_2016/day4.clj
+++ src/advent_of_code_2016/day4.clj
@@ -2,14 +2,15 @@
   (:require [clojure.string :as str]))
 
 ; Clears cruft out of the line and converts types
-(defn to-info [^String line]
+(defn- to-info [^String line]
   (let [[ln name sector hash] (re-find #"([a-z\-]+)-(\d+)\[([a-z]+)\]" line)]
-    [(str/replace name "-" "") (Long/parseLong sector) hash]))
+    [name (Long/parseLong sector) hash]))
 
-(defn valid
+(defn- valid
   [[name sector hash]]
-  (let [freq (frequencies name)
-        sort-letters (sort-by (juxt (comp - freq) identity) (distinct name))]
+  (let [clean-name (str/replace name "-" "")
+        freq (frequencies clean-name)
+        sort-letters (sort-by (juxt (comp - freq) identity) (distinct clean-name))]
         ; first sort by descending (negative) freq, then by forward natural order
     (= hash (apply str (take 5 sort-letters)))))
 
@@ -19,3 +20,17 @@
        (filter valid)
        (map second) ; Get the sector num
        (reduce +)))
+
+(defn- rotate [lower-letter amount]
+  (let [zbase (- (int lower-letter) (int \a))]
+    (char (+ (int \a) (mod (+ zbase amount) 26)))))
+
+(defn- decipher [[name sector hash]]
+  [(apply str (map #(if (= % \-) \space (rotate % sector)) name)) sector hash])
+
+(defn day4-2 [^String input]
+  (->> (str/split-lines input)
+       (map to-info)
+       (filter valid)
+       (map decipher)
+       (filter #(str/includes? (first %) "north"))))
