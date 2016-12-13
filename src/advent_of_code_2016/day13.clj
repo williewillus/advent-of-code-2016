c4f9ebe08516a83950d94b44fe0ca3551585c2a6
Vincent Lee <vincent.lee@outlook.com>
2016-12-13 11:35:35 -0600

    With some help, d13-1

---
+++ src/advent_of_code_2016/day13.clj
@@ -0,0 +1,44 @@
+(ns advent-of-code-2016.day13
+  (:require [clojure.data.priority-map :refer [priority-map]]))
+
+(def ^:private magic 1364)
+
+(defn- tile-type [[x y]]
+  (let [proc (+ (* x x) (* 3 x) (* 2 x y) y (* y y) magic)]
+    (if (even? (Integer/bitCount proc))
+      :open
+      :wall)))
+
+(defn- neighbors [[x y]]
+  (let [xp (inc x) xn (dec x)
+        yp (inc y) yn (dec y)]
+    (->> [[xp y] [x yn] [xn y] [x yp]]
+         (remove #(some neg? %))
+         (filter (fn [pos] (= :open (tile-type pos)))))))
+
+; Dijkstra in Clojure adapted from http://www.ummels.de/2014/06/08/dijkstra-in-clojure/
+
+(defn- map-vals [m f]
+  (into {}
+    (for [[k v] m]
+      [k (f v)])))
+
+(defn- remove-keys [m pred]
+  (select-keys m (remove pred (keys m))))
+
+(defn- dijkstra [start nb]
+  (loop [q (priority-map start 0)
+         seen {}]
+    (if-let [[node dist-from-start] (peek q)]               ; if there is more on the queue
+      (let [new-distances
+            (-> (zipmap (nb node) (repeat 1))               ; take neighbors (assuming edge cost 1)
+                (remove-keys seen)                          ; without the ones we've seen
+                (map-vals (partial + dist-from-start)))]    ; get total distance from start by adding on this edge's cost
+        (recur
+          (merge-with min (pop q) new-distances)            ; pop this node off the queue then update distances by merging in new-distances, taking the min value if there are conflicts
+          (assoc seen node dist-from-start)))               ; update the seen map with [node, dist from start]
+      seen)))                                               ; if queue is empty, seen has all info
+
+; End Dijkstra
+
+(defn day13-1 [] (get (dijkstra [1 1] neighbors) [31 39]))
