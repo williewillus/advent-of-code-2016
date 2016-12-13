8a2ea70cb4ee960af3f6e9366158fcd30c94b52a
Vincent Lee <vincent.lee@outlook.com>
2016-12-13 13:34:38 -0600

    d12-1 and d12-2

---
+++ src/advent_of_code_2016/day12.clj
@@ -0,0 +1,36 @@
+(ns advent-of-code-2016.day12
+  (:require [clojure.string :as str]))
+
+(defn- do-decode [^String insn]
+  (let [[op r1 r2] (str/split insn #"\s+")
+        inc-pc (fn [state] (update state :pc inc))]
+    (case op
+      "cpy" (comp inc-pc
+              (if (re-matches #"\d+" r1)
+                (fn [state] (assoc state (keyword r2) (Long/parseLong r1))) ; imm
+                (fn [state] (assoc state (keyword r2) ((keyword r1) state))))) ; reg
+      "inc" (comp inc-pc (fn [state] (update state (keyword r1) inc)))
+      "dec" (comp inc-pc (fn [state] (update state (keyword r1) dec)))
+      "jnz" (if (re-matches #"\d+" r1)
+              ; todo dedupe this code
+              (fn [state]                                    ; imm
+                (if (not (zero? (Long/parseLong r1)))
+                  (update state :pc (partial + (Long/parseLong r2)))
+                  (inc-pc state)))
+              (fn [state]                                   ; reg
+                (if (not (zero? ((keyword r1) state)))
+                  (update state :pc (partial + (Long/parseLong r2)))
+                  (inc-pc state)))))))
+
+(def ^:private decode (memoize do-decode))
+
+(defn- solve [^String input init-state]
+  (let [lines (str/split-lines input)
+        program-len (count lines)]
+    (loop [{pc :pc :as state} init-state]
+      (if (>= pc program-len)
+        state
+        (recur ((decode (nth lines pc)) state))))))
+
+(defn day12-1 [^String input] (solve input {:a 0 :b 0 :c 0 :d 0 :pc 0}))
+(defn day12-2 [^String input] (solve input {:a 0 :b 0 :c 1 :d 0 :pc 0}))
