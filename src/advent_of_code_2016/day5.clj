3587c01576b580ca943b000e057e5280e9e51537
Vincent Lee <vincent.lee@outlook.com>
2016-12-13 15:18:14 -0600

    Slow as hell d5-2

--- src/advent_of_code_2016/day5.clj
+++ src/advent_of_code_2016/day5.clj
@@ -1,27 +1,44 @@
 (ns advent-of-code-2016.day5
   (:import (java.security MessageDigest)))
 
-(def md5 (MessageDigest/getInstance "MD5"))
-(def hex-digits [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \a \b \c \d \e \f])
+(def ^:private md5 (MessageDigest/getInstance "MD5"))
+(def ^:private hex-digits [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \a \b \c \d \e \f])
+(def ^:private magic "wtnhxymk")
 
-(defn- compute [^String to-hash]
+; returns md5 digest if interesting, nil otherwise
+(defn- interesting-hash [to-hash]
   (.reset md5)
-  (.update md5 (.getBytes to-hash))
+  (.update md5 (.getBytes (str magic to-hash)))
 
   (let [digest (.digest md5)]
     ; the textual hash starts with 5 zeroes if the first 20 bits are 0
     ; first 20 bits => first 2 bytes (16 bits) and top half (4 bits) of the third
-    (if
+    (when
       (and
         (zero? (first digest))
         (zero? (second digest))
         (zero? (bit-and (unsigned-bit-shift-right (nth digest 2) 4) 0xF)))
-      (nth hex-digits (bit-and (nth digest 2) 0xF))
-      nil)))
+      digest)))
 
 (defn day5-1 []
   (->>
     (range)
-    (map #(compute (str "wtnhxymk" %)))
+    (map interesting-hash)
     (filter #(not (nil? %)))
+    (map #(nth hex-digits (bit-and (nth % 2) 0xF)))
     (take 8)))
+
+; todo this is slow as hell :(
+(defn day5-2 []
+  (loop [sltn (apply vector (repeat 8 nil))
+         iter 0
+         found 0]
+    (if (= found 8)
+      sltn
+      (if-let [hash (interesting-hash iter)]
+        (let [pos (bit-and (nth hash 2) 0xF)
+              chr (nth hex-digits (bit-and (unsigned-bit-shift-right (nth hash 3) 4) 0xF))]
+          (if (and (>= pos 0) (< pos 8) (nil? (nth sltn pos)))
+            (recur (assoc sltn pos chr) (inc iter) (inc found))
+            (recur sltn (inc iter) found)))
+        (recur sltn (inc iter) found)))))
