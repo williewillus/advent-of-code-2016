a50a6d2f7e37a84bc2e4911efb265a0179d74517
Vincent Lee <vincent.lee@outlook.com>
2016-12-15 16:16:54 -0600

    Combine d2-1 and d2-2

--- src/advent_of_code_2016/day2.clj
+++ src/advent_of_code_2016/day2.clj
@@ -1,28 +1,63 @@
-(ns advent-of-code-2016.day2
+(ns advent-of-code-2016.day2-2
   (:require [clojure.string :as str]))
 
-(def ^:private keypad-size 3)
+(def ^:private fancy-keypad
+  [nil nil 1 nil nil
+   nil  2  3  4  nil
+   5    6  7  8   9
+   nil  \A \B \C nil
+   nil nil \D nil nil])
 
-; row/col work on 0-based indices
-(defn- row [pos] (quot (dec pos) keypad-size))
-(defn- col [pos] (mod (dec pos) keypad-size))
+(def ^:private basic-keypad
+  [1 2 3
+   4 5 6
+   7 8 9])
 
-; these work on 1-based indices
-(defn- up [pos] (if (zero? (row pos)) pos (- pos keypad-size)))
-(defn- down [pos] (if (= (row pos) (- keypad-size 1)) pos (+ pos keypad-size)))
-(defn- left [pos] (if (zero? (col pos)) pos (dec pos)))
-(defn- right [pos] (if (= (col pos) (- keypad-size 1)) pos (inc pos)))
+(defn- keypad-size [kp] (int (Math/sqrt (count kp))))
 
-(defn- calc-pos [pos op]
+(defn- row [kp pos] (quot pos (keypad-size kp)))
+
+(defn- col [kp pos] (mod pos (keypad-size kp)))
+
+(defn- up [kp pos]
+  (let [new-pos (- pos (keypad-size kp))]
+    (if (or (zero? (row kp pos))
+            (nil? (nth kp new-pos)))
+      pos new-pos)))
+
+(defn- down [kp pos]
+  (let [new-pos (+ pos (keypad-size kp))]
+    (if (or (= (row kp pos) (dec (keypad-size kp)))
+            (nil? (nth kp new-pos)))
+      pos new-pos)))
+
+(defn- left [kp pos]
+  (let [new-pos (dec pos)]
+    (if (or (zero? (col kp pos))
+            (nil? (nth kp new-pos)))
+      pos new-pos)))
+
+(defn- right [kp pos]
+  (let [new-pos (inc pos)]
+    (if (or (= (col kp pos) (dec (keypad-size kp)))
+            (nil? (nth kp new-pos)))
+      pos new-pos)))
+
+(defn- calc-pos [kp pos op]
   (case op
-    \U (up pos)
-    \D (down pos)
-    \L (left pos)
-    \R (right pos)))
+    \U (up kp pos)
+    \D (down kp pos)
+    \L (left kp pos)
+    \R (right kp pos)))
 
-(defn- process-line [start-pos ^String line]
-  (println start-pos)
-  (reduce calc-pos start-pos line))
+(defn- process-line [kp start-pos ^String line]
+  (reduce (partial calc-pos kp) start-pos line))
 
-(defn day2-1 [^String input]
-  (reduce process-line 5 (str/split input #"\R+")))
+(defn- solve [kp start-idx input]
+  (map (partial nth kp)
+       (rest (reductions (partial process-line kp) start-idx (str/split input #"\R+")))))
+
+(defn day2-1 [^String input] (solve basic-keypad 4 input))
+
+(defn day2-2 [^String input] (solve fancy-keypad 10 input))
+
