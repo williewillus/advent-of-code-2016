8948d0772c65c41b966ad66d3137df04e04b826b
Vincent Lee <vincent.lee@outlook.com>
2016-12-14 18:11:20 -0600

    d14-2

--- src/advent_of_code_2016/day14.clj
+++ src/advent_of_code_2016/day14.clj
@@ -6,31 +6,37 @@
 
 (defn- md5-to-string [^bytes b]
   (let [bint (BigInteger. 1 b)]
-    (format "%x" bint)))
+    (format "%032x" bint)))
 
 (defn- md5-hash [^String s]
   (->> (.getBytes s)
        (.digest (MessageDigest/getInstance "MD5"))
        (md5-to-string)))
 
+(defn- many-hash [^String init]
+  (nth (iterate md5-hash init) 2017))
+
 (def ^:private get-info
   (memoize
-    (fn [idx]
+    (fn [hasher idx]
       ; given an idx, hashes magic+idx then returns
       ; 1: first char that appears 3 in a row (or nil)
       ; 2: set of what chars appear 5 in a row
-      (let [h (md5-hash (str magic idx))]
+      (let [h (hasher (str magic idx))]
         [(first (ffirst (re-seq #"([a-f\d])\1{2}" h)))
          (into #{} (map ffirst (re-seq #"([a-f\d])\1{4}" h)))]))))
 
-(defn- is-key [idx]
-  (let [[triple _] (get-info idx)]
+(defn- is-key [hasher idx]
+  (let [[triple _] (get-info hasher idx)]
     (and
       (some? triple)
       (->> (range (inc idx) (+ idx 1001))
-           (map get-info)
+           (map (partial get-info hasher))
            (map second)
            (some #(contains? % triple))))))
 
 (defn day14-1 []
-  (take 64 (filter is-key (range))))
+  (take 64 (filter (partial is-key md5-hash) (range))))
+
+(defn day14-2 []
+  (take 64 (filter (partial is-key many-hash) (range))))
