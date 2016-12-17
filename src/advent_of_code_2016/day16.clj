8907f860cc30efee82401870a90e3ece83cc12c7
Vincent Lee <vincent.lee@outlook.com>
2016-12-16 18:49:47 -0600

    checksum also uses strings to be consistent, and rename params

--- src/advent_of_code_2016/day16.clj
+++ src/advent_of_code_2016/day16.clj
@@ -3,11 +3,11 @@
 
 (def ^:private input "11110010111001001")
 
-(defn- dragon [xs]
-  (apply str xs \0 (map #(if (= \0 %) \1 \0) (str/reverse xs))))
+(defn- dragon [s]
+  (apply str s \0 (map #(if (= \0 %) \1 \0) (str/reverse s))))
 
-(defn- checksum [data]
-  (map (fn [[x y]] (if (= x y) \1 \0)) (partition 2 data)))
+(defn- checksum [s]
+  (apply str (map (fn [[x y]] (if (= x y) \1 \0)) (partition 2 s))))
 
 (defn- solve [required-size]
   (let [data (subs (->> (iterate dragon input)
@@ -15,9 +15,9 @@
                         (first))
                    0 required-size)
         check (->> (iterate checksum data)
-                   (drop-while #(even? (count %)))
+                   (remove #(even? (count %)))
                    (first))]
-    (apply str check)))
+    check))
 
 (defn day16-1 [] (solve 272))
 
