6e6dfd3472670ecf5abdb200d6ede83be818310b
Vincent Lee <vincent.lee@outlook.com>
2016-12-18 14:25:48 -0600

    Optimize d16-2 from 1+ minutes to 4s by using dirty imperative code and host arrays ;P

--- src/advent_of_code_2016/day16.clj
+++ src/advent_of_code_2016/day16.clj
@@ -1,23 +1,40 @@
 (ns advent-of-code-2016.day16
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import (java.util Arrays)))
 
-(def ^:private input "11110010111001001")
+(def ^:private ^booleans input (boolean-array (map #(= \1 %) "11110010111001001")))
 
-(defn- dragon [s]
-  (apply str s \0 (map #(if (= \0 %) \1 \0) (str/reverse s))))
+(set! *warn-on-reflection* true)
 
-(defn- checksum [s]
-  (apply str (map (fn [[x y]] (if (= x y) \1 \0)) (partition 2 s))))
+(defn- dragon [^booleans xs]
+  ; Imperative version of: (concat xs false (map not (reverse xs)))
+  (let [len (alength xs)
+        result (boolean-array (inc (* 2 len)))]
+    (System/arraycopy xs 0 result 0 len)
+    (aset result len false)
+    (doseq [i (range len)]
+      (let [reverse-idx (- (dec len) i)
+            shifted-reverse-idx (+ (inc len) reverse-idx)]
+        (aset result shifted-reverse-idx (not (aget xs i)))))
+    result))
+
+(defn- checksum [^booleans xs]
+  ; Imperative version of: (map (fn [[x y]] (= x y)) (partition 2 xs))
+  (let [len (alength xs)
+        result (boolean-array (int (/ len 2)))]
+    (doseq [i (range 0 len 2)]
+      (aset result (int (/ i 2)) (= (aget xs i) (aget xs (inc i)))))
+    result))
 
 (defn- solve [required-size]
-  (let [data (subs (->> (iterate dragon input)
-                        (remove #(< (count %) required-size))
-                        (first))
-                   0 required-size)
-        check (->> (iterate checksum data)
-                   (remove #(even? (count %)))
-                   (first))]
-    check))
+  (let [^booleans data (->> (iterate dragon input)
+                            (remove #(< (alength ^booleans %) required-size))
+                            (first))
+        truncated-data (Arrays/copyOf data (int required-size))
+        csum           (->> (iterate checksum truncated-data)
+                            (remove #(even? (alength ^booleans %)))
+                            (first))]
+    (apply str (map #(if % \1 \0) csum))))
 
 (defn day16-1 [] (solve 272))
 
