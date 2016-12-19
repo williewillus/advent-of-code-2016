4cac16194b981a7a210eefbf460eec59115d433d
Vincent Lee <vincent.lee@outlook.com>
2016-12-19 14:25:44 -0600

    Rename l to p in d19-2 since L looks a lot like 1 on github

--- src/advent_of_code_2016/day19.clj
+++ src/advent_of_code_2016/day19.clj
@@ -12,17 +12,17 @@
 
 (defn- solve-p2 [n]
   "day19-2: solved by hand while looking at /u/bblum's data table: http://pastebin.com/raw/8vdM5Jcz
-   Write n = 3^k + l, where k is as large as possible.
-   AKA, n = 3 ^ floor(log(n, 3)) + l
+   Write n = 3^k + p, where k is as large as possible.
+   AKA, n = 3 ^ floor(log(n, 3)) + p
    If n = 3^k, then w(n) = 3^k
-   If n <= 2(3^k), then w(n) = l
+   If n <= 2(3^k), then w(n) = p
    Otherwise, let n = 3^k+1 - m, then w(n) = 3^k+1 - 2m or more simply w(n) = n - m"
   (let [k (int (/ (Math/log n) (Math/log 3)))
         three-k (Math/pow 3 k)
-        l (- n three-k)]
+        p (- n three-k)]
     (cond
       (== n three-k) three-k
-      (<= n (* 2 three-k)) l
+      (<= n (* 2 three-k)) p
       :else (let [m (- (Math/pow 3 (inc k)) n)]
               (- n m)))))
 
