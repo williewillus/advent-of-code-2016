eff623b36689760c745b0013bc18d6a0e7b5bddd
Vincent Lee <vincent.lee@outlook.com>
2016-12-20 15:37:17 -0600

    d20-2 (and new d20-1 basically)

--- src/advent_of_code_2016/day20.clj
+++ src/advent_of_code_2016/day20.clj
@@ -1,14 +1,25 @@
 (ns advent-of-code-2016.day20
   (:require [clojure.string :as str]))
 
-(defn- line->pred [line]
-  (let [[_ m1 m2] (re-find #"(\d+)-(\d+)" line)
-        min (Long/parseLong m1)
-        max (Long/parseLong m2)]
-    (fn [x] (or (< x min) (> x max)))))
+(defn- line->pair [line]
+  (let [[_ min max] (re-find #"(\d+)-(\d+)" line)]
+    [(Long/parseLong min) (inc (Long/parseLong max))]))     ; work with inclusive-exclusive pairs
 
-(defn day20-1 [^String input]
-  (let [pred (apply every-pred (map line->pred (str/split input #"\R+")))]
-    (->> (range)
-         (filter pred)
-         (first))))
+(defn- rf [[acc banned allowed :as state]
+           pair]
+  (let [[a1 a2] acc
+        [b1 b2] pair]
+    (cond
+      (and (<= a1 b1) (>= a2 b2)) state                                   ; encompassing interval
+      (and (<= b1 a2) (>= b2 a2)) [[a1 b2] (+ banned (- b2 a2)) allowed]  ; direct expansion
+      (> b1 a2) (do (println (str "Gap at " a2 " of length " (- b1 a2)))
+                    [[a1 b2] (+ banned (- b2 a2)) (+ allowed (- b1 a2))]) ; there's a gap
+      :else (throw (IllegalStateException. "uh oh")))))
+
+(defn day20-2 [^String input]
+  (let [pairs (->> (str/split input #"\R+")
+                   (map line->pair)
+                   (cons [0 0])
+                   (sort))
+        [acc banned allowed] (reduce rf [[0 0] 0 0] pairs)]
+    (println "total allowed:" (- (Math/pow 2 32) (- banned allowed)))))
