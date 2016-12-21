8a115987d8bb970a2555378dc99cdc1f11483dfe
Vincent Lee <vincent.lee@outlook.com>
2016-12-21 10:33:39 -0600

    add project.clj

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject advent_of_code_2016 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2016 - williewillus"
+  :url "http://example.com/FIXME"
+  :license {:name "WTFPL-2.0"
+            :url "http://www.wtfpl.net/about/"}
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.priority-map "0.0.7"]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 ]
+  :main ^:skip-aot advent-of-code-2016.core ; todo actually make a core ns "launcher" for all days
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
