c7f9f05c0ff98975ba634428f40b484bc1b536b3
Vincent Lee <vincent.lee@outlook.com>
2016-12-22 12:55:07 -0600

    d22-2. kind of.

--- src/advent_of_code_2016/day22.clj
+++ src/advent_of_code_2016/day22.clj
@@ -2,14 +2,34 @@
   (:require [clojure.string :as str]))
 
 (defn- parse [line]
-  (let [[x y use avail] (map #(Long/parseLong %) (rest (re-find #"\/dev\/grid\/node-x(\d+)-y(\d+)\s+\d+T\s+(\d+)T\s+(\d+)T" line)))]
-    [[x y] {:used use :avail avail}]))
+  (let [[x y size use] (map #(Long/parseLong %) (rest (re-find #"\/dev\/grid\/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+\d+T" line)))]
+    [[x y] {:used use :size size}]))
+
+(defn- viable-pairs [nodes]
+  (for [[ak av :as a] nodes [bk bv :as b] nodes
+        :when (and (not= ak bk)
+                   (pos? (:used av))
+                   (<= (:used av) (- (:size bv) (:used bv))))]
+    [a b]))
 
 (defn day22-1 [^String input]
-  (let [nodes (apply hash-map (mapcat parse (drop 2 (str/split input #"\R+")))) ; drop 2 useless lines of input
-        pairs (for [[ak av :as a] nodes [bk bv :as b] nodes
-                    :when (and (not= ak bk)
-                               (pos? (:used av))
-                               (<= (:used av) (:avail bv)))]
-                [a b])]
-    (count pairs)))
+  (let [nodes (apply hash-map (mapcat parse (drop 2 (str/split input #"\R+"))))]
+    (count (viable-pairs nodes))))
+
+; this just prints it. solve manually :P
+(defn day22-2 [^String input]
+  (let [nodes (apply hash-map (mapcat parse (drop 2 (str/split input #"\R+"))))
+        max-x (apply max (map first (map key nodes)))
+        max-y (apply max (map second (map key nodes)))
+        target [max-x 0]]
+    (doseq [y (range (inc max-y))
+            x (range (inc max-x))]
+      (let [[k v] (find nodes [x y])]
+        (cond
+          (= k target) (print \G)
+          (= k [0 0]) (print \S)
+          (= (:used v) 0) (print \_)
+          (> (:size v) 200) (print \#)
+          :else (print \.)))
+      (when (= x max-x)
+        (newline)))))
